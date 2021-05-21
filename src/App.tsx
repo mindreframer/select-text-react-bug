826189b8c06be1d2e7d20947e5a043c6bb1337f3
Roman Heinrich <roman.heinrich@gmail.com>
2021-05-21 15:00:10 +0200

    FIX: add `{“\n”}` between span elements
    - https://github.com/facebook/react/issues/1643

--- src/App.tsx
+++ src/App.tsx
@@ -7,87 +7,169 @@ const App = () => {
       <div className="text-parts">
         <div className="inline text-part">
           <span>In</span>
+          {"\n"}
           <span>PARADISE</span>
+          {"\n"}
           <span>X,</span>
+          {"\n"}
           <span>Mr.</span>
+          {"\n"}
           <span>Camp’s</span>
+          {"\n"}
           <span>eighth-grade</span>
+          {"\n"}
           <span>class</span>
+          {"\n"}
           <span>consisting</span>
+          {"\n"}
           <span>of</span>
+          {"\n"}
           <span>26</span>
+          {"\n"}
           <span>students</span>
+          {"\n"}
           <span>was</span>
+          {"\n"}
           <span>surveyed</span>
+          {"\n"}
           <span>and</span>
+          {"\n"}
           <span>34.6</span>
+          {"\n"}
           <span>percent</span>
+          {"\n"}
           <span>of</span>
+          {"\n"}
           <span>the</span>
+          {"\n"}
           <span>students</span>
+          {"\n"}
           <span>reported</span>
+          {"\n"}
           <span>that</span>
+          {"\n"}
           <span>they</span>
+          {"\n"}
           <span>had</span>
+          {"\n"}
           <span>at</span>
+          {"\n"}
           <span>least</span>
+          {"\n"}
           <span>two</span>
+          {"\n"}
           <span>siblings.</span>
+          {"\n"}
           <span>The</span>
+          {"\n"}
           <span>average</span>
+          {"\n"}
           <span>eighth‑grade</span>
+          {"\n"}
           <span>class</span>
+          {"\n"}
           <span>size</span>
+          {"\n"}
           <span>in</span>
+          {"\n"}
           <span>the</span>
+          {"\n"}
           <span>state</span>
+          {"\n"}
           <span>is</span>
+          {"\n"}
           <span>26.</span>
+          {"\n"}
           <span>If</span>
+          {"\n"}
           <span>the</span>
+          {"\n"}
           <span>students</span>
+          {"\n"}
           <span>in</span>
+          {"\n"}
           <span>Mr.</span>
+          {"\n"}
           <span>Camp’s</span>
+          {"\n"}
           <span>class</span>
+          {"\n"}
           <span>are</span>
+          {"\n"}
           <span>representative</span>
+          {"\n"}
           <span>of</span>
+          {"\n"}
           <span>students</span>
+          {"\n"}
           <span>in</span>
+          {"\n"}
           <span>the</span>
+          {"\n"}
           <span>state’s</span>
+          {"\n"}
           <span>eighth-grade</span>
+          {"\n"}
           <span>classes</span>
+          {"\n"}
           <span>and</span>
+          {"\n"}
           <span>there</span>
+          {"\n"}
           <span>are</span>
+          {"\n"}
           <span>1,800</span>
+          {"\n"}
           <span>eighth-grade</span>
+          {"\n"}
           <span>classes</span>
+          {"\n"}
           <span>in</span>
+          {"\n"}
           <span>the</span>
+          {"\n"}
           <span>state,</span>
+          {"\n"}
           <span>which</span>
+          {"\n"}
           <span>of</span>
+          {"\n"}
           <span>the</span>
+          {"\n"}
           <span>following</span>
+          {"\n"}
           <span>best</span>
+          {"\n"}
           <span>estimates</span>
+          {"\n"}
           <span>the</span>
+          {"\n"}
           <span>number</span>
+          {"\n"}
           <span>of</span>
+          {"\n"}
           <span>eighth‑grade</span>
+          {"\n"}
           <span>students</span>
+          {"\n"}
           <span>in</span>
+          {"\n"}
           <span>the</span>
+          {"\n"}
           <span>state</span>
+          {"\n"}
           <span>who</span>
+          {"\n"}
           <span>have</span>
+          {"\n"}
           <span>fewer</span>
+          {"\n"}
           <span>than</span>
+          {"\n"}
           <span>two</span>
+          {"\n"}
           <span>siblings?</span>
+          {"\n"}
         </div>
       </div>
     </div>
